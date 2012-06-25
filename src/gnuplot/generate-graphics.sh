76b8b5e876cc12a32af33be981540bcee0af51c9
Adrien Mogenet <adrien.mogenet@gmail.com>
2012-06-25 18:56:36 +0200

    Added licence header and generate-graphics script

---
+++ src/gnuplot/generate-graphics.sh
@@ -0,0 +1,48 @@
+#!/bin/sh
+
+# This file is part of OpenGossip.
+#
+# This program is free software: you can redistribute it and/or modify it
+# under the terms of the GNU Lesser General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or (at your
+# option) any later version.  This program is distributed in the hope that it
+# will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty
+# of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser
+# General Public License for more details.  You should have received a copy
+# of the GNU Lesser General Public License along with this program.  If not,
+# see <http://www.gnu.org/licenses/>.
+
+if [ $# -ne 2 ]; then
+  echo "Usage: `basename $0` DAT_FOLDER OUTPUT_FOLDER"
+  exit 1
+fi
+
+DAT_FOLDER="$1"
+PNG_FOLDER="$2"
+GNUPLOT=`which gnuplot`
+
+if [ "$GNUPLOT" == "" ]; then
+  echo "Gnuplot not found. Check your \$PATH"
+  exit 2
+fi
+
+if [ ! -d $DAT_FOLDER ]; then
+  echo "Input folder ${DAT_FOLDER} does not exist"
+  exit 2
+fi
+
+if [ ! -d $PNG_FOLDER ]; then
+  echo "Output folder ${PNG_FOLDER} does not exist"
+  exit 2
+fi
+
+for file in $DAT_FOLDER/*.dat; do
+    echo "Processing ${file}"
+    filename=`basename ${file} .dat`
+    gnuplot <<EOF
+set term png size 800,480
+set output "$PNG_FOLDER/${filename}.png"
+set pointsize 0.1
+plot "${file}" w linespoints linecolor rgb "#336699" title "${filename}"
+EOF
+done
